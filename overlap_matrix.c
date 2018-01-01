28073b87f35375f9d4b46b29c8cfd7cca2dfa857
Demetrio Carrara <carrarademetrio@gmail.com>
2018-01-01 17:39:26 +0100

    Seems a reasonable and correct greedy algorithm
    Signed-off-by: Demetrio Carrara <carrarademetrio@gmail.com>

--- overlap_matrix.c
+++ overlap_matrix.c
@@ -2,6 +2,17 @@
 // USAGE: <executable> <input_file>
 // OUTPUT: <superstring>
 
+// n: numero reads
+// l: read length
+
+// complexity: O(n*n*l*l)
+
+/** TODOs:
+ *  better complexity
+ *  dynamic memory allocation
+ *  change data structure to SA if possible
+ */
+
 #include <stdio.h>
 #include <assert.h>
 #include <string.h>
@@ -15,8 +26,10 @@
 
 
 /*
- *  @return the max overlap between a suffix of read1 and a prefix of read2.
- *          returns strlen(read2) if and only if read2 is contained in read1.
+ * @return the max overlap between a suffix of read1 and a prefix of read2.
+ *         returns strlen(read2) if and only if read2 is contained in read1.
+ * 
+ * complexity: O(l*m) (l and m are reads length)
  */          
 int overlap(char* read1, char* read2) {
     int max_overlap = 0;
@@ -29,10 +42,10 @@ int overlap(char* read1, char* read2) {
             if (read1[i+j] != read2[j]) {
                 overlapping = false;
             } else {
+                // j is the current number of characters overlapping
                 j = j+1;
             }
         }
-        // j is the current number of characters overlapping
         // We have 3 cases for the cycle to exit:
         // 1) j == read2_length means that read2 is a substring of read1
         // this means that no other best solution can be found
@@ -42,7 +55,6 @@ int overlap(char* read1, char* read2) {
         // 2) i+j == read1_length means a prefix of read2 is a suffix of read1
         // Candidate to be the best local overlap
         if (i+j == read1_length) {
-            // update max_overlap
             max_overlap = j;
         }
         // 3) overlapping == false means that we must go on
@@ -54,7 +66,6 @@ int main (int argc, char** argv) {
     // <executable> <input_file>
     assert(argc > 1 && "Need exactly one additional argument (filename)");
 
-    // Read from file
     FILE * input_file;
     input_file = fopen(argv[1], "r");
     assert(input_file != NULL);
@@ -62,7 +73,6 @@ int main (int argc, char** argv) {
     // Assuming first line is the superstring so I can check the output
     char superstring[MAX_SUPERSTRING_LENGTH];
     assert(fgets(superstring, sizeof(superstring), input_file));
-    //printf("%s", superstring);
 
     // TODO: dynamic memory allocation
     char reads[MAX_READS][MAX_READ_LENGTH];
@@ -83,14 +93,13 @@ int main (int argc, char** argv) {
     // La matrice è n*n e l'overlap non è simmetrica: per ogni coppia (i,j) si calcola overlap di un suffisso di i come prefisso di j
     int overlap_matrix[n][n];
 
-    // COMPLESSITA'??? (da calcolare)
     for (int i = 0; i < n; i = i+1) {
         for (int j = 0; j < n; j = j+1) {
             // Do not calculate overlap between same string
             if (i != j)
                 overlap_matrix[i][j] = overlap(reads[i], reads[j]);
             else
-                overlap_matrix[i][i] = -1; 
+                overlap_matrix[i][j] = -1; 
         }
     }
 
@@ -109,6 +118,7 @@ int main (int argc, char** argv) {
         for (int i = 0; i < n; i = i+1) {
             if (used_strings[i] == true)
                 continue;
+            
             for (int j = 0; j < n; j = j+1) {
                 // do not count same strings and used ones
                 if (i == j || used_strings[j] == true)
@@ -121,11 +131,11 @@ int main (int argc, char** argv) {
                 }
             }
         }
-        printf("Max: %d in posizione %d %d\n", max, ii, jj);
+        //printf("Max: %d in posizione %d %d\n", max, ii, jj);
         // Ho trovato il max, fondo le stringhe i e j in modo che il suffisso di i sia prefisso di j
-        printf("Fondo %s + %s\n", reads[ii], reads[jj]);
+        //printf("Fondo %s + %s\n", reads[ii], reads[jj]);
         strcat(reads[ii], reads[jj]+max);
-        printf("Diventa %s\n", reads[ii]);
+        //printf("Diventa %s\n", reads[ii]);
         // in posizione ii avrò la nuova stringa fusa e jj la segno come posizione utilizzata
         used_strings[jj] = true;
 
@@ -141,7 +151,7 @@ int main (int argc, char** argv) {
     // sarà l'unica posizione settata a false
     for (int i = 0; i < n; i = i+1) {
         if (used_strings[i] == false)
-            printf("SUPERSTRINGA: %s\n", reads[i]);
+            printf("%s\n", reads[i]);
     }
     // Ricorro fino a che ho fuso tutte le stringhe 
     return 0;
